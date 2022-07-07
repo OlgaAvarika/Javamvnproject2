1a935fe3f9488a61ad229fde5e011a64c34b4fa7
Ольга Евстифеева <olga1989nikol@rambler.ru>
2022-07-07 16:19:16 +0300

    Files fixed

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,8 +4,7 @@ public class SQRService {
     public int calcSqrt(int num1, int num2) {
         int numberSquares = 0;
         for (int i = 10; i <= 99; i++) {
-            int x = i * i;
-            if (x >= num1 && x <= num2) {
+            if (i * i >= num1 && i * i <= num2) {
                 numberSquares++;
             }
         }
